18e63839c7199bf5ff0a01bd2d4a9b09d245862c
f.karim <f.karim@pythys.com>
2022-09-23 21:03:17 +0600

    server cors updated

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
 // app.use(cors());
-const corsOptions = {
-  origin: "https://books-library-client.vercel.app",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://books-library-client.vercel.app",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wanl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
