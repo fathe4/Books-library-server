0d5de006e10542eee5ca6f3a501ec36a716e5f98
f.karim <f.karim@pythys.com>
2022-09-23 21:43:25 +0600

    cors update

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
-// app.use(cors());
-// const corsOptions = {
-//   origin: "https://books-library-client.vercel.app",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
 app.use(cors());
 app.use(express.json());
 
